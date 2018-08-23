57c4f2d3cb72dc775620675badbe712f181f1480
jhonsef <jrp.ctt@gmail.com>
2018-08-23 11:21:12 -0300

    [settings](filials): retira métodos e atributos da classe
    Retira da classe de filiais coisas que não eram responsabilidade.

--- src/resources/Filials.java
+++ src/resources/Filials.java
@@ -6,80 +6,26 @@ public class Filials {
 	
 	private Integer id;
 	private String name;
-	private ArrayList<Products> ListProducts;//Lembrar que os produtos não podem ser repetidos
+	private Stock stock;
 	
 	public Filials(Integer id, String name) {
 		this.setId(id);
 		this.setName(name);
-		this.ListProducts = new ArrayList<Products>();
+		this.stock = new Stock();
 	}
 
-	public Filials(Integer id, String name, ArrayList<Products> listProducts) {
+	public Filials(Integer id, String name, Stock stock) {
 		this.id = id;
 		this.name = name;
-		ListProducts = listProducts;
+		this.stock = stock;
 	}
-
+	
+	//Gets [Genéricos]
 	public Integer getId() { return id; }
 	public String getName() { return name; }
-	public ArrayList<Products> getListProducts() { return ListProducts; }
+	public ArrayList<Products> getStock() { return this.stock.getProducts(); }
 
-
+	//Sets [Genéricos]
 	public void setId(Integer id) { this.id = id; }
 	public void setName(String name) { this.name = name; }
-	public void setListProducts(ArrayList<Products> listProducts) { ListProducts = listProducts; }
-	
-	/**
-	 * Inserir UM produto na lista de produtos da filial
-	 * */
-	public boolean insertProduct(Products product) {
-		
-		for(int i=0; i < this.ListProducts.size(); i++) {
-			if (this.ListProducts.get(i).getId() == product.getId()) { 
-				return false;
-			}
-		}
-		this.ListProducts.add(product);
-		return true;
-	}
-	
-	/**
-	 * Inserir VÁRIOS produtos na lista de produtos da filial
-	 * */
-	public ArrayList<Products> insertProducts(ArrayList<Products> products) {
-		
-		ArrayList<Products> ctrl = new ArrayList<Products>();
-		boolean find = false;
-		for (int i=0; i < products.size(); i++) {
-			for (int j=0; j < this.ListProducts.size(); j++) {
-				if (products.get(i).getId() == this.ListProducts.get(j).getId()) {
-					find = true;
-					break;
-				}
-			}
-			if (find) {
-				ctrl.add(products.get(i));
-			}else {
-				this.ListProducts.add(products.get(i));
-			}
-			find = false;
-		}
-		
-		return ctrl;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
