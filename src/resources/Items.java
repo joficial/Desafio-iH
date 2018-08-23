cb6d82b614541408aee7ee0f8751dc877f45e56f
jhonsef <jrp.ctt@gmail.com>
2018-08-23 11:19:03 -0300

    [start](items): cria e inicializa a classe items
    Cria e adiciona os elementos internos da classe item

---
+++ src/resources/Items.java
@@ -0,0 +1,30 @@
+package resources;
+
+import java.util.ArrayList;
+
+public class Items {
+	
+	private Products product;
+	private Integer in = 0;
+	private Integer out = 0;
+	
+	public Items(Products product, Integer in, Integer out) {
+		this.setProduct(product);
+		this.setIn(in);
+		this.setOut(out);
+	}
+	
+	public Products getProduct() { return this.product; }
+	public Integer getIn() { return in; }
+	public Integer getOut() { return out; }
+
+	public void setProduct(Products product) { this.product = product; }
+	public void setIn(Integer in) { this.in = in; }
+	public void setOut(Integer out) { this.out = out; }	
+	
+	//Fix
+	public Products fixProduct() {
+		this.product.insertVolume(this.in - this.out);
+		return this.product;
+	}
+}
